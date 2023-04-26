2201e14875b1ab85c9c9cf3e930bb2c921ecff8c
lucastteles <98498654+lucastteles@users.noreply.github.com>
2023-04-26 17:36:32 -0300

    Adicionar arquivos de projeto.

---
+++ BellaVini,MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BellaVini_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
